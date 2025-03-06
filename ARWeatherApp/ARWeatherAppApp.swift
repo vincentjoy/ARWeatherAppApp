7cf694d34a75c16c176b5fdd40f72c34bad254a9
Vincent Joy <vincentjoyhere@gmail.com>
2025-03-06 14:14:44 +0530

    Initial Commit

---
+++ ARWeatherApp/ARWeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ARWeatherAppApp.swift
+//  ARWeatherApp
+//
+//  Created by Vincent Joy on 06/03/25.
+//
+
+import SwiftUI
+
+@main
+struct ARWeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ARWeatherApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ARWeatherApp
+//
+//  Created by Vincent Joy on 06/03/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
