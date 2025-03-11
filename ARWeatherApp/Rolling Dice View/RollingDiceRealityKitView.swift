9839b632e4788d50e255f6de08d71a125d17f19a
Vincent Joy <vincentjoyhere@gmail.com>
2025-03-11 19:19:42 +0530

    palced the dice

--- ARWeatherApp/Rolling Dice View/RollingDiceRealityKitView.swift
+++ ARWeatherApp/Rolling Dice View/RollingDiceRealityKitView.swift
@@ -2,8 +2,35 @@
 import ARKit
 import RealityKit
 import SwiftUI
+import FocusEntity
 
 struct RollingDiceRealityKitView: UIViewRepresentable {
+    
+    class Coordinator: NSObject, ARSessionDelegate {
+        weak var view: ARView?
+        var focusEntity: FocusEntity?
+
+        func session(_ session: ARSession, didAdd anchors: [ARAnchor]) {
+            guard let view = self.view else { return }
+            debugPrint("Anchors added to the scene: ", anchors)
+            self.focusEntity = FocusEntity(on: view, style: .classic(color: .yellow))
+        }
+        
+        @objc func handleTap() {
+            guard let view = self.view, let focusEntity = self.focusEntity else { return }
+
+            // Create a new anchor to add content to
+            let anchor = AnchorEntity()
+            view.scene.anchors.append(anchor)
+
+            // Add a dice entity
+            let diceEntity = try! ModelEntity.loadModel(named: "Dice")
+            diceEntity.scale = [0.1, 0.1, 0.1]
+            diceEntity.position = focusEntity.position
+
+            anchor.addChild(diceEntity)
+        }
+    }
     
     func makeUIView(context: Context) -> ARView {
         let view = ARView()
@@ -13,6 +40,18 @@ struct RollingDiceRealityKitView: UIViewRepresentable {
         let config = ARWorldTrackingConfiguration()
         config.planeDetection = [.horizontal]
         session.run(config)
+        
+        // Handle ARSession events via delegate
+        context.coordinator.view = view
+        session.delegate = context.coordinator
+        
+        // Handle taps
+        view.addGestureRecognizer(
+            UITapGestureRecognizer(
+                target: context.coordinator,
+                action: #selector(Coordinator.handleTap)
+            )
+        )
         
         // Add coaching overlay which guides the user until the first plane is found
         let coachingOverlay = ARCoachingOverlayView()
@@ -27,6 +66,10 @@ struct RollingDiceRealityKitView: UIViewRepresentable {
         #endif
         
         return view
+    }
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator()
     }
     
     func updateUIView(_ view: ARView, context: Context) {
